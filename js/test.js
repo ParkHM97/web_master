867d674d6087ecb82c42fa007fc733309154aad4
pc <pc@email.com>
2024-11-13 09:25:34 +0900

    test

---
+++ js/test.js
@@ -0,0 +1,29 @@
+// test.js
+// 1. 1 ~ 10까지 숫자의 합 
+// 결과 : 1 ~ 10까지의 합은 55 입니다. 
+
+// Q1 
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//   sum = sum + i; 
+
+// } document.write(`1부터 10까지의 합은 ${sum} 입니다.`)
+
+// Q2
+// 구구단 4단 출력 
+
+// const four = 4; 
+// for (let i = 1; i < 10; i++) {
+//   console.log(`${four} * ${i} = ${four * i}`);
+// }
+
+// Q3 배열의 요소 활용, 합 구하기 (홀수의 합은 ? 입니다.)
+
+// let numAry = [23, 66, 92, 71, 52, 49, 11];
+// let sum = 0;
+
+// for (let i = 0; i < numAry.length; i++) {
+//   if (numAry[i] % 2 == 1) {
+//     sum += numAry[i];
+//   }
+// } console.log(`홀수의 합은 ${sum} 입니다.`);
