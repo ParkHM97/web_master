51db4100c5da0fd6d08f32f5def432633d49c1a3
pc <pc@email.com>
2024-11-22 17:48:37 +0900

    movie

---
+++ movie/member.js
@@ -0,0 +1,34 @@
+const memberList = [{
+  uname: '01사용자',
+  userid: 'user01',
+  userpw: 'user01'
+}, 
+{
+  uname: '02사용자',
+  userid: 'user02',
+  userpw: 'user02'
+},
+{
+  uname: '03사용자',
+  userid: 'user03',
+  userpw: 'user03'
+}]
+
+const movieList = [{
+  moviename: 'Sound of Music',
+  days: '1969년 10월',
+  score: '평점 : 9.60',
+  image: 'sound.img'
+  },
+  {
+  moviename: '하울의 움직이는 성',
+  days: '2004년 11월',
+  score: '평점 : 9.35',
+  image: 'howl.img'
+  },
+  {
+  moviename: '오늘 밤, 세계에서 이 사랑이 사라진다 해도',
+  days: '2022년 07월',
+  score: '평점 : 8.75',
+  image: 'today.img'
+  }]
